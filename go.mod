390fe04e2e98df6c825a1ba1b76d23fd81a1107e
tetsuya <tetsuya@users.noreply.github.com>
2020-03-05 12:30:54 +0900

    Run 'Hello, world' on echo server

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/tetsuya/echo-hello-world
 go 1.14
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/labstack/echo/v4 v4.1.15 // indirect
 	golang.org/x/crypto v0.0.0-20200302210943-78000ba7a073 // indirect
 	golang.org/x/net v0.0.0-20200301022130-244492dfa37a // indirect
--- main.go
+++ main.go
@@ -1,6 +1,27 @@
 package main
-import "fmt"
 
-func main(){
-    fmt.Printf("Hello, world\n")
+import (
+  "net/http"
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+  // Echo instance
+  e := echo.New()
+
+  // Middleware
+  e.Use(middleware.Logger())
+  e.Use(middleware.Recover())
+
+  // Routes
+  e.GET("/", hello)
+
+  // Start server
+  e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+  return c.String(http.StatusOK, "Hello, World!")
 }
